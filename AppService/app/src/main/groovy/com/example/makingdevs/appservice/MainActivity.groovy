905dbe424a19522ba37be39a2e9e32dacfcd7dbc
LeoVergara1 <leovergara.dark@gmail.com>
2017-01-26 12:57:47 -0600

    Vincule elementos con .groovy

--- AppService/app/src/main/groovy/com/example/makingdevs/appservice/MainActivity.groovy
+++ AppService/app/src/main/groovy/com/example/makingdevs/appservice/MainActivity.groovy
@@ -1,13 +1,25 @@
 package com.example.makingdevs.appservice;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import groovy.transform.CompileStatic;
 
+@CompileStatic
 public class MainActivity extends AppCompatActivity {
+    Button mButtonSend
+    Button mButtonDelete
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mButtonDelete = (Button) findViewById(R.id.button_delete)
+        mButtonSend = (Button) findViewById(R.id.button_delete)
     }
 }
