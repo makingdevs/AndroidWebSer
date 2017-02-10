1afaa7e21c77ae8574d92ff89f7ece1c1b434b0d
LeoVergara1 <leovergara.dark@gmail.com>
2017-02-10 10:15:04 -0600

    Modificaciones de diseño

---
+++ AppService/app/src/main/groovy/com/makingdevs/network/Api.groovy
@@ -0,0 +1,18 @@
+package com.makingdevs.network;
+
+/**
+ * Created by makingdevs on 10/02/17.
+ */
+
+public class Api {
+
+    String account
+    String amount
+    String concept
+
+     Api(String account, String amount, String concept){
+
+
+    }
+
+}
